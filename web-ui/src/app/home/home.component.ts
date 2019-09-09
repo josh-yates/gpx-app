ccd0b61f1e2bdd412b28cf31ad97e67441f25485
Joshua Yates <josht.yates@btinternet.com>
2019-09-09 21:53:06 +0100

    Add change event for file input

--- web-ui/src/app/home/home.component.ts
+++ web-ui/src/app/home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent {
   constructor() { }
 
-  ngOnInit() {
+  public fileChange($event: Event): void {
+    const fileInputElement = $event.target as HTMLInputElement;
+
+    console.log(fileInputElement.files);
   }
 
 }
