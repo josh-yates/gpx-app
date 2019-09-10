f63f4d6924ff4b023ecb23db6cd3d22359c5a9d9
Joshua Yates <josht.yates@btinternet.com>
2019-09-10 21:31:25 +0100

    Setup filereader for attached file

--- web-ui/src/app/home/home.component.ts
+++ web-ui/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -13,14 +13,35 @@ export class HomeComponent {
     '.gpx'
   ];
 
+  public isLoading = false;
   public selectedFile: File = null;
+  private readonly fileReader = new FileReader();
 
   public fileChange($event: Event): void {
     const fileInputElement = $event.target as HTMLInputElement;
 
-    const isValidFile = fileInputElement.files.length && this.acceptedExtensions.indexOf(`.${(fileInputElement.files[0]).name}`) >= 0;
+    const isValidFile = fileInputElement.files.length &&
+     this.acceptedExtensions.indexOf(`.${(fileInputElement.files[0]).name.split('.').pop()}`) >= 0;
 
     this.selectedFile = isValidFile ? fileInputElement.files[0] : null;
   }
 
+  public submit(): void {
+    if (!this.selectedFile) {
+      return;
+    }
+
+    this.isLoading = true;
+
+    this.fileReader.removeEventListener('loadend', () => this.onFileLoadEnd());
+    this.fileReader.addEventListener('loadend', () => this.onFileLoadEnd());
+
+    this.fileReader.readAsText(this.selectedFile);
+  }
+
+  private onFileLoadEnd(): void {
+    this.isLoading = false;
+    const xmlFile = this.fileReader.result;
+    console.log(xmlFile);
+  }
 }
